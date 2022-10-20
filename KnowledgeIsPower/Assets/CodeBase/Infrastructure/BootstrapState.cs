c8eca068a7f364da07fb8d0d2c3af5b697ebdb75
Vadya999 <nhynh7@gmail.com>
2022-10-20 21:57:08 +0600

    11
    11

---
+++ KnowledgeIsPower/Assets/CodeBase/Infrastructure/BootstrapState.cs
@@ -0,0 +1,49 @@
+﻿using System.Net.Mime;
+using CodeBase.Services.Input;
+using UnityEditor.SceneManagement;
+
+namespace CodeBase.Infrastructure
+{
+    public class BootstrapState : IState
+    {
+        private const string Initial = "Initial";
+        private readonly GameStateMachine _stateMachine;
+        private readonly Game.SceneLoader _sceneLoader;
+
+        public BootstrapState(GameStateMachine stateMachine, Game.SceneLoader sceneLoader)
+        {
+            _stateMachine = stateMachine;
+            _sceneLoader = sceneLoader;
+        }
+
+        public void Enter()
+        {
+            //RegisterServices();
+            
+            _sceneLoader.Load(Initial, EnterLoadLevel);
+        }
+
+        private void EnterLoadLevel()
+        {
+            
+        }
+
+        public void Exit()
+        {
+            
+        }
+
+        /*private void RegisterServices()
+        {
+            Game.InputService = SceneSetup;
+        }
+        
+        private static void RegisterInputInput()
+        {
+            if (MediaTypeNames.Application.isEditor)
+                InputService = new StandaloneInputService();
+            else
+                InputService = new MobileInputService();
+        }*/
+    }
+}
---
+++ KnowledgeIsPower/Assets/CodeBase/Infrastructure/Game.SceneLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace CodeBase.Infrastructure
+{
+    public partial class Game
+    {
+    }
+}
