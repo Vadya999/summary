f032f57bf317ca9d0c97b6fdcfab0585ae81b253
Vadya999 <nhynh7@gmail.com>
2022-12-07 18:34:22 +0400

    add testt

---
+++ TTest/Assets/Scripts/PlayerImpulse.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+    public class PlayerImpulse : MonoBehaviour
+    {
+        public bool isDamage;
+        
+        [SerializeField] private float impulseDistance;
+        [SerializeField] private float impulseDamageTime;
+
+        private CharacterController _controller;
+
+        private void Awake() => 
+            _controller = GetComponent<CharacterController>();
+
+        private void Update() => 
+            CheckLkmDown();
+
+        private void CheckLkmDown()
+        {
+            if (Input.GetKeyDown(KeyCode.Mouse0))
+            {
+                MoveImpulse();
+                DamageTime();
+            }
+        }
+
+        private void MoveImpulse()
+        {
+            _controller.Move(transform.forward * impulseDistance);
+        }
+
+        private IEnumerator DamageTime()
+        {
+            isDamage = true;
+            yield return new WaitForSeconds(impulseDamageTime);
+            isDamage = false;
+            yield break;
+        }
+    }
